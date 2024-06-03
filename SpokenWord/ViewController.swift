d24ca0609faa783c6637b652abc5c5829fc89ea6
duc <duc@appiphany.com.au>
2024-06-03 18:02:47 +0700

    Init project

---
+++ SpokenWord/ViewController.swift
@@ -0,0 +1,244 @@
+/*
+See the LICENSE.txt file for this sample’s licensing information.
+
+Abstract:
+The root view controller that provides a button to start and stop recording, and which displays the speech recognition results.
+*/
+
+import UIKit
+import Speech
+
+public class ViewController: UIViewController, SFSpeechRecognizerDelegate {
+    // MARK: Properties
+    
+    private let speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "en-US"))!
+    
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    
+    private var recognitionTask: SFSpeechRecognitionTask?
+    
+    private let audioEngine = AVAudioEngine()
+    
+    @IBOutlet var textView: UITextView!
+    
+    @IBOutlet var recordButton: UIButton!
+    
+    @IBOutlet weak var playButton: UIButton!
+    
+    var audioFile: AVAudioFile?
+    var audioPlayer: AVAudioPlayer?
+    var audioFileURL: URL?
+    // MARK: Custom LM Support
+
+    @available(iOS 17, *)
+    private var lmConfiguration: SFSpeechLanguageModel.Configuration {
+        let outputDir = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+        let dynamicLanguageModel = outputDir.appendingPathComponent("LM")
+        let dynamicVocabulary = outputDir.appendingPathComponent("Vocab")
+        return SFSpeechLanguageModel.Configuration(languageModel: dynamicLanguageModel, vocabulary: dynamicVocabulary)
+    }
+    
+    // MARK: UIViewController
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Disable the record buttons until authorization has been granted.
+        recordButton.isEnabled = false
+    }
+    
+    override public func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        // Configure the SFSpeechRecognizer object already
+        // stored in a local member variable.
+        speechRecognizer.delegate = self
+        
+        // Make the authorization request.
+        SFSpeechRecognizer.requestAuthorization { authStatus in
+
+            // Divert to the app's main thread so that the UI
+            // can be updated.
+            OperationQueue.main.addOperation {
+                switch authStatus {
+                case .authorized:
+                    if #available(iOS 17, *) {
+                        Task.detached {
+                            do {
+                                let assetPath = Bundle.main.path(forResource: "CustomLMData", ofType: "bin", inDirectory: "customlm/en_US")!
+                                let assetUrl = URL(fileURLWithPath: assetPath)
+                                try await SFSpeechLanguageModel.prepareCustomLanguageModel(for: assetUrl,
+                                                                                           clientIdentifier: "com.apple.SpokenWord",
+                                                                                           configuration: self.lmConfiguration)
+                            } catch {
+                                NSLog("Failed to prepare custom LM: \(error.localizedDescription)")
+                            }
+                            await MainActor.run { self.recordButton.isEnabled = true }
+                        }
+                    } else {
+                        self.recordButton.isEnabled = true
+                    }
+                case .denied:
+                    self.recordButton.isEnabled = false
+                    self.recordButton.setTitle("User denied access to speech recognition", for: .disabled)
+                    
+                case .restricted:
+                    self.recordButton.isEnabled = false
+                    self.recordButton.setTitle("Speech recognition restricted on this device", for: .disabled)
+                    
+                case .notDetermined:
+                    self.recordButton.isEnabled = false
+                    self.recordButton.setTitle("Speech recognition not yet authorized", for: .disabled)
+                    
+                default:
+                    self.recordButton.isEnabled = false
+                }
+            }
+        }
+    }
+    
+    
+    @IBAction func onPlayRecording(_ sender: Any) {
+//        do {
+//                audioPlayer = try AVAudioPlayer(contentsOf: audioFileURL!)
+//                audioPlayer?.play()
+//            } catch {
+//                print("Error playing audio file: \(error.localizedDescription)")
+//            }
+        
+        playAudioFile()
+    }
+    
+    func playAudioFile() {
+           guard let audioFilePath = Bundle.main.path(forResource: "sample", ofType: "m4a") else {
+               print("Audio file not found")
+               return
+           }
+           
+           let audioFileURL = URL(fileURLWithPath: audioFilePath)
+           
+           do {
+               audioPlayer = try AVAudioPlayer(contentsOf: audioFileURL)
+               audioPlayer?.prepareToPlay()
+               audioPlayer?.play()
+           } catch {
+               print("Error initializing AVAudioPlayer: \(error.localizedDescription)")
+           }
+       }
+
+    
+    private func startRecording() throws {
+        
+        // Cancel the previous task if it's running.
+        if let recognitionTask = recognitionTask {
+            recognitionTask.cancel()
+            self.recognitionTask = nil
+        }
+        
+        // Configure the audio session for the app.
+        let audioSession = AVAudioSession.sharedInstance()
+        try audioSession.setCategory(.playAndRecord, mode: .default, options: [.defaultToSpeaker, .allowBluetooth, .interruptSpokenAudioAndMixWithOthers])
+        try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
+        let inputNode = audioEngine.inputNode
+
+        // Create and configure the speech recognition request.
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+        guard let recognitionRequest = recognitionRequest else { fatalError("Unable to created a SFSpeechAudioBufferRecognitionRequest object") }
+        recognitionRequest.shouldReportPartialResults = true
+        
+        // Keep speech recognition data on device
+        if #available(iOS 13, *) {
+            recognitionRequest.requiresOnDeviceRecognition = true
+            if #available(iOS 17, *) {
+                recognitionRequest.customizedLanguageModel = self.lmConfiguration
+            }
+        }
+        
+        // Create a recognition task for the speech recognition session.
+        // Keep a reference to the task so that it can be canceled.
+        recognitionTask = speechRecognizer.recognitionTask(with: recognitionRequest) { result, error in
+            var isFinal = false
+            
+            if let result = result {
+                // Update the text view with the results.
+                self.textView.text = result.bestTranscription.formattedString
+                isFinal = result.isFinal
+            }
+            
+            if error != nil || isFinal {
+                // Stop recognizing speech if there is a problem.
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+                
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+                
+                self.recordButton.isEnabled = true
+                self.recordButton.setTitle("Start Recording", for: [])
+            }
+        }
+
+        // Configure the microphone input.
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        
+        // setup audio
+        let documentsPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        audioFileURL = documentsPath.appendingPathComponent("recordedAudio.caf")
+
+        do {
+            audioFile = try AVAudioFile(forWriting: audioFileURL!, settings: recordingFormat.settings)
+        } catch {
+            fatalError("Unable to create AVAudioFile: \(error.localizedDescription)")
+        }
+        
+        try audioEngine.inputNode.setVoiceProcessingEnabled(true)
+        try audioEngine.outputNode.setVoiceProcessingEnabled(true)
+        
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer: AVAudioPCMBuffer, when: AVAudioTime) in
+            self.recognitionRequest?.append(buffer)
+            
+//            do {
+//                    try self.audioFile?.write(from: buffer)
+//               } catch {
+//                    print("Error writing buffer to file: \(error.localizedDescription)")
+//            }
+        }
+        
+        audioEngine.prepare()
+        try audioEngine.start()
+        
+        // Let the user know to start talking.
+        textView.text = "(Go ahead, I'm listening)"
+    }
+    
+    // MARK: SFSpeechRecognizerDelegate
+    
+    public func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        if available {
+            recordButton.isEnabled = true
+            recordButton.setTitle("Start Recording", for: [])
+        } else {
+            recordButton.isEnabled = false
+            recordButton.setTitle("Recognition Not Available", for: .disabled)
+        }
+    }
+    
+    // MARK: Interface Builder actions
+    
+    @IBAction func recordButtonTapped() {
+        if audioEngine.isRunning {
+            audioEngine.stop()
+            recognitionRequest?.endAudio()
+            recordButton.isEnabled = false
+            recordButton.setTitle("Stopping", for: .disabled)
+            self.audioFile = nil
+        } else {
+            do {
+                try startRecording()
+                recordButton.setTitle("Stop Recording", for: [])
+            } catch {
+                recordButton.setTitle("Recording Not Available", for: [])
+            }
+        }
+    }
+}
+
